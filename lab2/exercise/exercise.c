f2579e1c06ee132f085f98c9d398bee8029df2cd
Andrew SZE-TO <zkwokleung@gmail.com>
2023-09-20 18:45:35 +0800

    add: lab2 templates

---
+++ lab2/exercise/exercise.c
@@ -0,0 +1,22 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+/*
+
+Under the same folder, file input.txt.
+Each line contains a positive integer number and we guarantee the number is smaller than 1000.
+You need to read the number in each line, add it by one, and write them line by line to another file called output.txt
+
+NOTE: Please don't hardcode the results in your program because we change the content in input.txt when grading.
+
+TIP: You can use sscanf to convert char array to int and sprintf to convert int to char array. 
+
+*/
+int main(){
+
+}
