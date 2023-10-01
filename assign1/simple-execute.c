604b08f419091480b2b876285b1797d1758575eb
Andrew SZETO <56270363+zkwokleung@users.noreply.github.com>
2023-10-01 23:13:12 +0800

    fix: assign1 unnecessary loop

--- assign1/simple-execute.c
+++ assign1/simple-execute.c
@@ -10,6 +10,30 @@
 #include <errno.h>
 #include <unistd.h>
 
+#pragma region My Debug Functions
+// Enums
+#define DEBUG_LEVEL_NONE 100
+#define DEBUG_LEVEL_ALL -1
+#define DEBUG_LEVEL_LOG 0
+#define DEBUG_LEVEL_WARN 1
+#define DEBUG_LEVEL_ERROR 2
+
+// * Change the log level here
+#define DEBUG_LEVEL DEBUG_LEVEL_NONE
+
+#define debug_log(...)                                                                                                 \
+    if (DEBUG_LEVEL <= DEBUG_LEVEL_LOG)                                                                                \
+        printf(__VA_ARGS__);
+
+#define debug_warn(...)                                                                                                \
+    if (DEBUG_LEVEL <= DEBUG_LEVEL_WARN)                                                                               \
+        printf(__VA_ARGS__);
+
+#define debug_error(...)                                                                                               \
+    if (DEBUG_LEVEL <= DEBUG_LEVEL_ERROR)                                                                              \
+        printf(__VA_ARGS__);
+#pragma endregion
+
 // You can adjust the maximum number of pipe here!
 #define MAX_PIPE_COUNT 100
 
@@ -42,6 +66,8 @@ int shell_execute(char **args, int argc)
             pipe_count++;
             args[i] = NULL;
             pipe_indecies[pipe_count] = i + 1;
+
+            debug_log("? Found pipe at arg %d\n", i);
         }
     }
 
@@ -50,28 +76,26 @@ int shell_execute(char **args, int argc)
     if ((child_pid = fork()) < 0)
     {
         // Error
-        perror("Error while forking main process \n");
+        perror("! Error while forking main process \n");
     }
     else if (child_pid == 0)
     {
         int child_pid = -1, wait_return = -1, status = -1;
-        int pipefd[2];
-        pipe(pipefd);
 
         // Create child processes and execute commands
-        for (i = 0; i <= pipe_count; i++)
+        for (i = 0; i < pipe_count; i++)
         {
             // Create pipe
             if (pipe(pipefds[i]) < 0)
             {
-                perror("Error in pipe() \n");
+                perror("! Error in pipe() \n");
                 exit(EXIT_FAILURE);
             }
 
             if ((child_pid = fork()) < 0)
             {
                 // Error
-                perror("Error in fork() \n");
+                perror("! Error in fork() \n");
                 exit(EXIT_FAILURE);
             }
             else if (child_pid == 0)
@@ -92,17 +116,20 @@ int shell_execute(char **args, int argc)
         {
             if ((wait_return = wait(&status)) < 0)
             {
-                perror("Error in wait() of command pipe\n");
+                perror("! Error in wait() of command pipe\n");
                 exit(EXIT_FAILURE);
             }
         }
+
+        debug_log("? Process %d executing command: %s at arg %d\n", i, args[pipe_indecies[pipe_count - i]],
+                  pipe_indecies[pipe_count - i]);
 
         // Redirect stdin to get the ouput from the previous command (output of the child process)
         if (i < pipe_count)
         {
             if (dup2(pipefds[i][0], STDIN_FILENO) < 0)
             {
-                perror("Error in dup2(STDIN_FILENO) \n");
+                perror("! Error in dup2(STDIN_FILENO) \n");
                 exit(EXIT_FAILURE);
             }
         }
@@ -112,7 +139,7 @@ int shell_execute(char **args, int argc)
             // Redirect stdout to send the ouput to the next command (parent this process)
             if (dup2(pipefds[i - 1][1], STDOUT_FILENO) < 0)
             {
-                perror("Error in dup2(STDOUT_FILENO) \n");
+                perror("! Error in dup2(STDOUT_FILENO) \n");
                 exit(EXIT_FAILURE);
             }
         }
@@ -127,18 +154,18 @@ int shell_execute(char **args, int argc)
         // Execute command
         if (execvp(args[pipe_indecies[pipe_count - i]], args + pipe_indecies[pipe_count - i]) < 0)
         {
-            perror("Error in execvp() \n");
+            perror("! Error executing command ");
             exit(EXIT_FAILURE);
         }
 
-        // Finish executing a command
+        // * Finish executing a command
     }
     else
     {
         // Main Parent process
         // Wait for the commands to finish execute
         if ((wait_return = wait(&status)) < 0)
-            perror("Error in wait() of main process\n");
+            perror("! Error in wait() of main process\n");
     }
 
     return 0;
