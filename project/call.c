d478e85890d3f0b7b518dabed408010bdc54fbc4
Andrew SZE-TO <zkwokleung@gmail.com>
2023-10-14 22:28:55 +0800

    update: proj read_t solution

--- project/call.c
+++ project/call.c
@@ -83,25 +83,130 @@ int open_t(char *pathname)
 
 int read_t(int i_number, int offset, void *buf, int count)
 {
-    int read_bytes;
+    int read_bytes = 0;
+    int byte_to_read = 0;
+    inode *ip;
+    int byte_to_read_remain;
+    int byte_in_file_remain;
+    int current_block_index = 0;
+    int current_block;
+    int current_offset;
+    int total_offset;
+
+    // Check if the count is valid
+    if (count > MAX_FILE_SIZE)
+    {
+        printf("!!! Error: count is larger than MAX_FILE_SIZE\n");
+        return 0;
+    }
+
+    // Check if the offset is valid
+    if (offset > MAX_FILE_SIZE)
+    {
+        printf("!!! Error: offset is larger than MAX_FILE_SIZE\n");
+        return 0;
+    }
 
     // Open the HD file
     int fd = open(HD, O_RDWR);
     if (fd < 0)
     {
-        printf("Error: open()\n");
+        printf("Error openning HD\n");
         return -1;
     }
 
-    // Read the inode
-    inode *ip = read_inode(fd, i_number);
-    if (ip == NULL)
+    // Read the inode from disk
+    if (lseek(fd, I_OFFSET + i_number * sizeof(inode), SEEK_SET) < 0)
     {
-        printf("Error: read_inode()\n");
+        printf("!!! Error ruuning lseek() while reading inode\n");
         return -1;
     }
 
-    // Read the data
+    ip = malloc(sizeof(inode));
+    if (read(fd, ip, sizeof(inode)) != sizeof(inode))
+    {
+        printf("!!! Error running read() while reading inode\n");
+        return -1;
+    }
+
+    byte_in_file_remain = ip->f_size - offset;
+    byte_to_read_remain = count;
+    current_offset = offset;
+
+    current_block = ip->direct_blk[0];
+
+    while (byte_to_read_remain > 0 && current_block != -1 && byte_in_file_remain > 0)
+    {
+        if (current_offset < BLK_SIZE)
+        {
+            // ! The offset is within a block
+
+            // Calculate the number of byte to read in the current block
+            byte_to_read = byte_to_read_remain;
+            if (byte_to_read > BLK_SIZE - current_offset)
+            {
+                byte_to_read = BLK_SIZE - current_offset;
+            }
+
+            // Calculate the number of byte to read in the file
+            if (byte_to_read > byte_in_file_remain)
+            {
+                byte_to_read = byte_in_file_remain;
+            }
+
+            byte_in_file_remain -= byte_to_read;
+            byte_to_read_remain -= byte_to_read;
+
+            // Read the block
+            if (lseek(fd, D_OFFSET + current_block * BLK_SIZE + current_offset, SEEK_SET) < 0)
+            {
+                printf("!!! Error ruuning lseek() while reading block\n");
+                return -1;
+            }
+
+            if (read(fd, buf + read_bytes, byte_to_read) != byte_to_read)
+            {
+                printf("!!! Error running read() while reading block\n");
+                return -1;
+            }
+            read_bytes += byte_to_read;
+
+            total_offset += byte_to_read;
+            current_offset = BLK_SIZE;
+        }
+        else
+        {
+            // ! The offset is not within a block
+
+            // Move to the next block
+            if (++current_block_index < 2)
+            {
+                current_block = ip->direct_blk[current_block_index];
+            }
+            else
+            {
+                int indirect_blk_buf;
+                // Read the indirect block
+                if (lseek(fd, D_OFFSET + ip->indirect_blk * BLK_SIZE + (current_block_index - 2) * sizeof(int),
+                          SEEK_SET) < 0)
+                {
+                    printf("!!! Error ruuning lseek() while reading indirect block\n");
+                    return -1;
+                }
+
+                if (read(fd, &current_block, sizeof(int)) != sizeof(int))
+                {
+                    printf("!!! Error running read() while reading indirect block\n");
+                    return -1;
+                }
+            }
+            current_offset -= BLK_SIZE;
+        }
+    }
+
+    // Release
+    close(fd);
+    free(ip);
 
     return read_bytes;
 }
