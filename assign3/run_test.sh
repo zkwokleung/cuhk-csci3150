01011b6e1061971839a40a711ea396e90e13b467
Andrew SZETO <zkwokleung@gmail.com>
2023-11-06 00:06:31 +0800

    update: test script

--- assign3/run_test.sh
+++ assign3/run_test.sh
@@ -1,3 +1,47 @@
+# -----------------------------------------------------------------------------
+#   Author: Andrew SZE-TO
+#   Last Modified: 2023-11-06
+# -----------------------------------------------------------------------------
+#  Description:
+#   This script is used to test your code for Bonus Assignment 3.
+# ? Before running your code, you should structure your directory as follows:
+#   .
+#   ├── Bonus-Assign3-Code
+#   │   ├── Makefile
+#   |   ...
+#   │   └── TaskScheduler.h
+#   ├── Bonus-Assign3-Testcases
+#   │   ├── testcase1
+#   │   │   ├── input.txt
+#   │   │   └── output.log
+#   │   ├── testcase2
+#   │   ...
+#   │   └── testcase5
+#   │       ├── input.txt
+#   │       └── output.log
+#   └── run_test.sh
+# 
+# ? Then, you can run this script to test your code with the following command:
+#   Linux/MacOS:
+#   $ ./run_test.sh
+# 
+#   Windows:
+#   $ bash run_test.sh
+#
+# * In case you cannot run this script, you can run the following commands:
+#   Linux/MacOS:
+#   $ sudo chmod 777 run_test.sh
+# 
+# * After running this script, a folder named "test" will be created.
+#   You can check the output of your code in test/output.log
+#   If your code is correct, the output of your code will be the same as the expected output
+#   in Bonus-Assign3-Testcases/testcase1/output.log
+#
+#   If your code is incorrect, the output of your code will be different from the expected output.
+#   You may check test/diff.log for more information.
+#   The diff.log is generated with the following command:
+#   $ diff ./test/output.log ./test/expected.log > ./test/diff.log
+
 rm -rf test
 mkdir test
 
@@ -20,6 +64,7 @@ do
 
     pid=$!
 
+    # ! If you are running on a slow computer, you may need to increase the sleep time
     sleep 0.5
 
     if ps -p $pid > /dev/null
